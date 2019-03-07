fa727cb602dc3f88d78db79c92bff3b7888e2b72
Thembinkosi <thembinkosi.klein@gmail.com>
2019-03-07 20:01:29 +0200

    add customer

--- controllers/Customers.js
+++ controllers/Customers.js
@@ -164,37 +164,7 @@ CustomersController.prototype.addAddress = function(request, reply) {
 // [POST] /customers
 CustomersController.prototype.store = function(request, reply) {
     try {
-        var values          = {};
-        values.name         = request.payload.name;
-        values.email        = request.payload.email;
-        values.telephone    = request.payload.telephone;
-        values.fax          = (request.payload.fax) ? request.payload.fax : "";
-        values.address_1    = request.payload.address_1;
-        values.address_2    = (request.payload.address_2) ? request.payload.address_2 : "";
-        values.city         = request.payload.city;
-        values.postcode     = request.payload.postcode;
-        values.region_id    = request.payload.region_id;
-        values.country_id   = request.payload.country_id;
-        values.address      = values.address_1+" "+values.address_2+" "+values.city+" "+values.postcode;
-
-        // hard-coded fields/values
-        var password             = 'Replogic'; // general initial password
-        values.customer_group_id = 1;
-        values.store_id          = 0;
-        values.language_id       = 1;
-        values.address_id        = 0; // will be updated on after the address insert later on (below)
-        values.firstname         = values.name;
-        values.lastname          = values.name;
-        values.newsletter        = 0;
-        values.ip                = 0;
-        values.custom_field      = "";
-        values.status            = 1;
-        values.approved          = 0;
-        values.safe              = 0;
-        values.token             = "";
-        values.code              = "";
-        
-        this.customersModel.addCustomer(values, reply);
+        this.customersModel.addCustomer(request.payload, reply);
     } catch (e) {
         reply(Boom.badRequest(e.message));
     }
