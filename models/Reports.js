cf91626dc00f3c4a8151a23046792e80465e155f
Thembinkosi <thembinkosi.klein@gmail.com>
2019-03-13 19:26:36 +0200

    reports

--- models/Reports.js
+++ models/Reports.js
@@ -85,7 +85,7 @@ ReportsModel.prototype.getReportById = function(id, reply) {
             if (results.length) {
                 if (results[0].sql.length > 0) {
                     var query = that.getQuery(results[0].sql);
-                    var query = query.split(`{filters}`).join(`DATE_FORMAT(tr.date_added,"%Y-%m-%d")>=DATE_FORMAT(${filters.start_date},"%Y-%m-%d") AND DATE_FORMAT(tr.date_added,"%Y-%m-%d")<=DATE_FORMAT(${filters.end_date},"%Y-%m-%d")`);
+                    query = that.str_replace(query, `<filters>`, `WHERE DATE_FORMAT(tr.date_added,"%Y-%m-%d")>=DATE_FORMAT(${that.filters.start_date},"%Y-%m-%d") AND DATE_FORMAT(tr.date_added,"%Y-%m-%d")<=DATE_FORMAT(${that.filters.end_date},"%Y-%m-%d")`);
                     that.db.set(query);
                     connection.query(that.db.get(),
                     function (error, results, fields) {
@@ -119,6 +119,10 @@ ReportsModel.prototype.getReportById = function(id, reply) {
         }
     });
 };
+
+ReportsModel.prototype.str_replace = function(string, search, replace) {
+    return string.split(search).join(replace);
+}
 
 /**
  * Get query
